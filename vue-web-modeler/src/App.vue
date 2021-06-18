8c34d1b0e4c3f34f1c22bf81e75cb5dcc5df53f8
Dominik Horn <dominik.horn93@gmail.com>
2021-06-18 11:27:47 +0200

    feat: add vue js example

---
+++ vue-web-modeler/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div style="height: 100vh" id="app">
+    <Modeler/>
+  </div>
+</template>
+
+<script>
+import Modeler from "@/components/Modeler";
+
+export default {
+  name: 'App',
+  components: {
+    Modeler
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
